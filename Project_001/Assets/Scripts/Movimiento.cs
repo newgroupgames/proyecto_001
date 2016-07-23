7479adebd5f78f2c69830fb4401905b250708e64
newgroupgames <newgroup.games@gmail.com>
2016-07-23 00:31:59 -0300

    commit project
    unity project commited

---
+++ Project_001/Assets/Scripts/Movimiento.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Saltar))]
+public class Movimiento : MonoBehaviour {
+
+    float velX = 0f;
+    float limiteVelMax = 4f;
+    float limiteVelMin;
+    Rigidbody2D rb;
+    Vector2 limiteDeVelocidad;
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Start()
+    {
+        rb.freezeRotation = true;
+        limiteVelMin = limiteVelMax * -1;
+    }
+	void Update () {
+        velX = Input.GetAxis("Horizontal")*limiteVelMax;
+        LimitarVelocidad();
+        VoltearPersonaje();
+    }
+    void FixedUpdate()
+    {
+        rb.AddRelativeForce(new Vector2(velX, 0), ForceMode2D.Impulse);
+    }
+    void LimitarVelocidad()
+    {
+        if (rb.velocity.x > limiteVelMax)
+        {
+            limiteDeVelocidad = rb.velocity;
+            limiteDeVelocidad.x = limiteVelMax;
+            rb.velocity = limiteDeVelocidad;
+        }
+        else if (rb.velocity.x < limiteVelMin)
+        {
+            limiteDeVelocidad = rb.velocity;
+            limiteDeVelocidad.x = limiteVelMin;
+            rb.velocity = limiteDeVelocidad;
+        }
+    }
+    void VoltearPersonaje()
+    {
+        if (velX>0)
+        {
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        }
+        else if (velX<0)
+        {
+            transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        }
+    }
+    public void CambiarVelocidad(float f)
+    {
+        limiteVelMax = f;
+        limiteVelMin = f * -1;
+    }
+   
+}
---
+++ Project_001/Assets/Scripts/Saltar.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Saltar : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Transform pies;
+    Vector2 dobleSalto;
+
+    Movimiento movCs;
+    Transform body;
+    Vector2 pointVector;
+
+    float fuerzaDeSalto = 8f;// 1 cuadro es 6.5f
+    float gravity;                  //para wall jump
+    bool puedeSaltar = true;        
+    bool puedeSaltarDoble = true;   //para doble salto
+    bool saltando = false;          
+    public bool tieneDobleSalto = true;    //habilita el doble salto
+    public bool tieneWallJump = true;      //habilita el wall jump
+    bool enLaPared = false;         
+    int layerSuelo;                 
+    int layerPisable;               
+    int layers;                     
+    void Awake()
+    {
+        GameObject child = new GameObject();
+        child.name = "Pies";
+        child.transform.SetParent(transform);
+        child.transform.position = new Vector2(transform.position.x, transform.position.y - GetComponent<Renderer>().bounds.size.y / 2);
+        pies = transform.FindChild("Pies");
+        rb = GetComponent<Rigidbody2D>();
+        GameObject childB = new GameObject();
+        childB.name = "Body";
+        childB.transform.SetParent(transform);
+        childB.transform.position = new Vector2(transform.position.x, transform.position.y);
+        body = transform.FindChild("Body");
+        pointVector = new Vector2(0.55f, 0.25f);
+        rb.gravityScale = 2;
+        movCs = GetComponent<Movimiento>();
+    }
+    void Start()
+    {
+        layerSuelo = 1 << 8;
+        layerPisable = 1 << 12;
+        layers = layerSuelo | layerPisable;
+        gravity = rb.gravityScale;
+    }
+    void Update()
+    {
+        puedeSaltar = Physics2D.OverlapCircle(pies.position, 0.2f, layers);
+        enLaPared = Physics2D.OverlapArea((Vector2)transform.position - pointVector, (Vector2)transform.position + pointVector, layerSuelo);
+        Collider2D col = Physics2D.OverlapArea((Vector2)transform.position - pointVector, (Vector2)transform.position + pointVector, layerSuelo);
+        saltando = Input.GetButtonDown("Jump");
+        if (puedeSaltar && tieneDobleSalto)
+            puedeSaltarDoble = true;
+        if (saltando && puedeSaltar)
+            SaltoSimple();
+        if (saltando && !puedeSaltar && puedeSaltarDoble && tieneDobleSalto)
+            SaltoDoble();
+        if (enLaPared && tieneWallJump)
+        {
+            gameObject.SendMessage("CambiarVelocidad", 1);
+            puedeSaltar = false;
+            if (tieneDobleSalto)
+                puedeSaltarDoble = false;
+            if (saltando)
+            {
+                rb.velocity = Vector2.zero;
+                SaltoEnLaPared(col);
+            }
+        }
+        else
+        {
+            gameObject.SendMessage("CambiarVelocidad", 4);//numero # hardcodeado de script movimiento del personaje
+        }
+    }
+    void SaltoSimple()
+    {
+        rb.AddRelativeForce(Vector2.up * fuerzaDeSalto, ForceMode2D.Impulse);
+    }
+    void SaltoDoble()
+    {
+        puedeSaltarDoble = false;
+        dobleSalto = new Vector2(rb.velocity.x,0);
+        rb.velocity = dobleSalto;
+        rb.AddRelativeForce(Vector2.up * fuerzaDeSalto, ForceMode2D.Impulse);
+    }
+    void SaltoEnLaPared(Collider2D col)
+    {
+        if (col.transform.position.x > transform.position.x)
+        {
+            rb.AddForce((Vector2.left * 20f) + (Vector2.up * fuerzaDeSalto*1.2f), ForceMode2D.Impulse);
+        }
+        else if (col.transform.position.x < transform.position.x)
+        {
+            rb.AddForce((Vector2.right * 20f) + (Vector2.up * fuerzaDeSalto*1.2f), ForceMode2D.Impulse);
+        }
+    }
+    public bool GetPuedeSaltar() { return puedeSaltar; }
+    public void SetPuedeSaltar(bool v) { puedeSaltar = v; }
+}
